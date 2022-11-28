536d8612ca4e5ffa35bcab97bbbc348bbe923786
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-28 12:39:41 -0500

    Working on login / logout with cookies

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser")
 const app = express();
 const port = process.env.PORT || 8181;
 const shortenUrl = require("./helpers/shortenUrl");
@@ -15,11 +16,30 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+
+
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  const templateVars= {username: 'aingaran'}
+  res.render("pages/index", templateVars);
+});
+
+
+//auth
+
+app.post("/login", (req, res) => {
+  const {username} = req.body;
+  res.cookie('username', username, { expires: new Date(Date.now() + 900000), httpOnly: true });
+  res.send("cookie")
+
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie('username', "");
+  res.redirect("/")
 });
 
 app.get("/url/new", (req, res) => {
@@ -30,11 +50,12 @@ app.post("/url/new", (req, res) => {
   const longUrl = req.body.longUrl;
   const shortUrl = shortenUrl();
   urlDatabase[shortUrl] = longUrl;
-  res
-    .status(201)
-    .send(
-      `Long Url: ${longUrl} and Short Url: http://localhost:8181/url/${shortUrl}`
-    );
+  res.redirect(`/url/${shortUrl}`)
+  // res
+  //   .status(201)
+  //   .send(
+  //     `Long Url: ${longUrl} and Short Url: http://localhost:8181/url/${shortUrl}`
+  //   );
 });
 
 app.post("/url/:id/delete", (req, res) => {
@@ -42,8 +63,19 @@ app.post("/url/:id/delete", (req, res) => {
   delete urlDatabase[id];
   res.send(urlDatabase)
   console.log(urlDatabase);
+});
 
+app.post("/url/:id/edit", (req, res) => {
+  const {id, newUrl} = req.body;
+  urlDatabase[id] = newUrl;
+  res.redirect(`/url/${id}`)
+  // console.log(urlDatabase);
+});
 
+app.get("/url/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const templateVars = { username: username, id: id, longURL: urlDatabase[id] };
+  res.render("pages/urls_edit", templateVars);
 });
 
 app.get("/url/:id", (req, res) => {
