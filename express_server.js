2d75b3eb896ef5efe79eea819e3c71425e0839f7
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-28 16:40:10 -0500

    updated

--- express_server.js
+++ express_server.js
@@ -1,65 +1,79 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 8181;
 const shortenUrl = require("./helpers/shortenUrl");
-require("./helpers/createUser");
+// const userRoutes = require("./routes/user.routes")
+// const urlRoutes = require("./routes/url.routes")
+// require("./helpers/createUser");
+
+const db = require("./db/users.json");
+
+console.log(db.userId);
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// app.use("/user", userRoutes)
+// app.use("/url", urlRoutes)
+
+// let username = null;
+
+// if (res.cookie())
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  const templateVars= {username: ''}
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+
   res.render("pages/index", templateVars);
 });
 
-
 //auth
 app.post("/login", (req, res) => {
-  const {username} = req.body;
-  res.cookie('username', username, { expires: new Date(Date.now() + 900000), httpOnly: true });
-  res.send("cookie")
-
+  const { username } = req.body;
+  res.cookie("username", username, {
+    expires: new Date(Date.now() + 900000),
+    httpOnly: true,
+  });
+  res.redirect("/");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username')
-  res.redirect("/")
+  res.clearCookie("username");
+  res.redirect("/");
 });
 
 app.get("/register", (req, res) => {
-  new User;
-  res.render("pages/register")
+  new User();
+  res.render("pages/register", templateVars);
+});
 
-})
-
-app.post("/register", (req, res) => {
-
-})
-
-
-
+app.post("/register", (req, res) => {});
 
 app.get("/url/new", (req, res) => {
-  res.render("pages/urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("pages/urls_new", templateVars);
 });
 
 app.post("/url/new", (req, res) => {
   const longUrl = req.body.longUrl;
   const shortUrl = shortenUrl();
   urlDatabase[shortUrl] = longUrl;
-  res.redirect(`/url/${shortUrl}`)
+  res.redirect(`/url/${shortUrl}`);
   // res
   //   .status(201)
   //   .send(
@@ -68,28 +82,30 @@ app.post("/url/new", (req, res) => {
 });
 
 app.post("/url/:id/delete", (req, res) => {
+  
   const id = req.body.id;
   delete urlDatabase[id];
-  res.send(urlDatabase)
-  console.log(urlDatabase);
+  res.redirect(`/urls`);
 });
 
 app.post("/url/:id/edit", (req, res) => {
-  const {id, newUrl} = req.body;
+
+  const { id, newUrl } = req.body;
   urlDatabase[id] = newUrl;
-  res.redirect(`/url/${id}`)
+  res.redirect(`/url/${id}`);
   // console.log(urlDatabase);
 });
 
 app.get("/url/:id/edit", (req, res) => {
+
   const id = req.params.id;
-  const templateVars = { username: username, id: id, longURL: urlDatabase[id] };
+  const templateVars = {     username: req.cookies["username"], id: id, longURL: urlDatabase[id] };
   res.render("pages/urls_edit", templateVars);
 });
 
 app.get("/url/:id", (req, res) => {
   const id = req.params.id;
-  const templateVars = { id: id, longURL: urlDatabase[id] };
+  const templateVars = { username: req.cookies["username"], id: id, longURL: urlDatabase[id] };
   res.render("pages/urls_show", templateVars);
 });
 
@@ -100,7 +116,7 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("pages/urls_index", templateVars);
 });
 
